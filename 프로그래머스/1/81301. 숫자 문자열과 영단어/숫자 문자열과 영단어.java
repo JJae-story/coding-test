bf77da9d90f2bfd05899738003353558442a8197
JJae <jjae.3.lee@gmail.com>
2024-10-22 11:17:25 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.12 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(String s) {
+        String[] number = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+        for (int i = 0; i < number.length; i++) {
+            s = s.replace(number[i], Integer.toString(i));
+        }
+
+        return Integer.parseInt(s);
+    }
+}
