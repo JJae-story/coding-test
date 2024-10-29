731a3b45507809516399f9dd71840baa631ed3f2
JJae <jjae.3.lee@gmail.com>
2024-10-29 13:11:08 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 2.22 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
@@ -2,16 +2,19 @@ import java.util.*;
 
 class Solution {
     public String solution(String s) {
-        Map<Character, Integer> map = new TreeMap<>();
+        Map<Character, Integer> map = new HashMap<>();
         
         for (char c : s.toCharArray()) {
             map.put(c, map.getOrDefault(c, 0) + 1);
         }
         
+        List<Character> keys = new ArrayList<>(map.keySet());
+        Collections.sort(keys);
+        
         String answer = "";
-        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
-            if (entry.getValue() == 1) {
-                answer += entry.getKey();
+        for (Character key : keys) {
+            if (map.get(key) == 1) {
+                answer += key;
             }
         }
         return answer;
