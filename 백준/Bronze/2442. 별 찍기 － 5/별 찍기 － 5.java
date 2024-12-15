07e3b786f892c3c25fb963a8d4d67e6cbc95b184
JJae <jjae.3.lee@gmail.com>
2024-12-15 16:12:40 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 108 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
@@ -1,22 +1,25 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		// 별의 개수 입력 받기 ( 1 ~ 100 )
-		int N = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine().trim());
 
-		for (int i = 1; i <= N; i++) {
-            // 공백 찍기
-			for (int j = 1; j <= N - i; j++) {
-			    System.out.print(" ");
+        StringBuilder output = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            // 공백 추가
+            for (int j = 1; j <= N - i; j++) {
+                output.append(" ");
             }
-            // 별 찍기
+            // 별 추가
             for (int k = 1; k <= 2 * i - 1; k++) {
-                System.out.print("*");
+                output.append("*");
             }
-            System.out.println();
-		}
-        sc.close();
-	}
+            // 줄 바꿈
+            output.append("\n");
+        }
+        System.out.print(output);
+    }
 }
